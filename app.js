cc8e7eb6aef307cd06e0d68b1ef69d7307556752
litera123 <92157611+litera123@users.noreply.github.com>
2023-05-28 22:55:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const cena = document.getElementById("cena")
+
+function obliczanie(dane) {
+    let cena_koncowa;
+    if ( dane.ilosc){
+        cena_koncowa=parseFloat(dane.ilosc.value*3);
+    }
+    if( dane.rozek[0].checked){
+        cena_koncowa+=parseFloat(dane.rozek[0].value)
+    }
+    if( dane.rozek[1].checked){
+        cena_koncowa+=parseFloat(dane.rozek[1].value)
+    }
+    if( dane.rozek[2].checked){
+        cena_koncowa+=parseFloat(dane.rozek[2].value)
+    }
+    if( dane.add[0].checked){
+        cena_koncowa+=parseFloat(dane.add[0].value)
+    }
+    if( dane.add[1].checked){
+        cena_koncowa+=parseFloat(dane.add[1].value)
+    }
+    if( dane.add[2].checked){
+        cena_koncowa+=parseFloat(dane.add[2].value)
+    }
+    if( dane.vat[0].checked){
+        cena_koncowa=cena_koncowa;
+    }
+    if( dane.vat[1].checked){
+        cena_koncowa*=parseFloat(dane.vat[1].value)
+    }
+    if( dane.vat[2].checked){
+        cena_koncowa*=parseFloat(dane.vat[2].value)
+    }
+    if( dane.dis[0].checked){
+        cena_koncowa=cena_koncowa;
+    }
+    if( dane.dis[1].checked){
+        cena_koncowa*=parseFloat(dane.dis[1].value)
+    }
+    if( dane.dis[2].checked){
+        cena_koncowa*=parseFloat(dane.dis[2].value)
+    }
+
+
+    cena.innerHTML = cena_koncowa.toFixed(2) + " pln";
+    
+}
+  
